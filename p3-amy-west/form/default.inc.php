439e591c5b3c6e069ce3e87e1e55754a7097964e
amywest1 <87080188+amywest1@users.noreply.github.com>
2021-08-06 18:36:14 -0400

    Add files via upload

---
+++ p3-amy-west/form/default.inc.php
@@ -0,0 +1,68 @@
+<style type="text/css">
+  * {
+    margin: 0;
+    border: 0;
+    padding: 0;
+  }
+
+  header {
+    background: white;
+    color: black;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    z-index: 99;
+    text-align: center;
+  }
+
+  header nav {
+    display: inline-block;
+  }
+
+  header nav li {
+    display: inline-block;
+    vertical-align: top;
+    font-size: 36px;
+    padding: 0 5px;
+  }
+
+  li:not(.logo) {
+    margin-top: 40px;
+  }
+
+  .logo {
+    margin: 0 30px;
+  }
+  header nav a {
+    text-decoration: none;
+    font-family: 'Jost', sans-serif;
+    color: black;
+    transition: 0.3s;
+    padding: 0 10px;
+  }
+
+  h1{
+    font-family: 'Trocchi', serif;
+  }
+  p {
+    font-family: 'Jost', sans-serif;
+  }
+</style>
+
+<header>
+    <nav>
+      <ul>
+        <li><a href="#">Meet Matt</a></li>
+        <li><a href="#">Order Thicc</a></li>
+        <li><a href="#">Current Drop</a></li>
+        <li class="logo"><a href="#"><img src="images/mfc-logo.jpg" alt="Matt's Fat Cookies Logo" width='150'></a></li>
+        <li><a href="http://amywest.net/mattsfatcookieorderform/">Order Now</a></li>
+        <li><a href="https://www.instagram.com/mattsfatcookies/" target="_blank">Instagram</a></li>
+        <li><a href="#">Instant Cookies</a></li>
+      </ul>
+    </nav>
+
+<div class="container">
+  <h1>Sorry something went wrong with your submission.</h1>
+  <p>Please try again.</p>
+</div>
